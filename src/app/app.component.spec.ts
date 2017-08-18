088f67aaaad4dc1ad26e3b49bb12f2b0f933ebe7
Steve Zaske <github@zaske.com>
2017-08-18 10:19:42 -0700

    ability to add new animals

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
-
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        FormsModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -27,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Animals');
   }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { Animal } from './animal';
+import { AnimalDataService } from './animal-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AnimalDataService]
 })
 export class AppComponent {
   title = 'app';
+
+  newAnimal: Animal = new Animal();
+
+  constructor(private animalDataService: AnimalDataService){
+  }
+
+  addAnimal() {
+    this.animalDataService.addAnimal(this.newAnimal);
+    this.newAnimal = new Animal();
+  }
+
+  removeAnimal(animal) {
+    this.animalDataService.deleteAnimalById(animal.id);
+  }
+
+  get animals() {
+    return this.animalDataService.getAllAnimals();
+  }
+
+
 }
